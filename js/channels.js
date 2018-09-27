0e9b1e4039a7792d76ac0883aaa5ddfeadc82d51
GFeghaly <geoker1987@gmail.com>
2018-09-27 22:25:43 +0300

    challenge7&8 first issue

---
+++ js/channels.js
@@ -0,0 +1,20 @@
+var yummy = { name: '#Yummy', createdOn: new Date(2016,3,16), createdBy:'minus.plus.yummy',starred:'false',expiresIn:'100',messageCount:'999' };
+
+console.log(yummy);
+
+
+var SevenContinents = { name: '#SevenContinents', createdOn: new Date(2016,3,16), createdBy:'cheeses.yard.applies',starred:'true',expiresIn:'100',messageCount:'999' };
+
+console.log(SevenContinents);
+
+var KillerApp = { name: '#KillerApp', createdOn: new Date(2016,3,16), createdBy:'minus.plus.yummy',starred:'false',expiresIn:'100',messageCount:'999' };
+
+console.log(KillerApp);
+
+var FirstPersonOnMars = { name: '#FirstPersonOnMars', createdOn: new Date(2016,3,16), createdBy:'minus.plus.yummy',starred:'true',expiresIn:'100',messageCount:'999' };
+
+console.log(FirstPersonOnMars);
+
+var Octoberfest = { name: '#Octoberfest', createdOn: new Date(2016,3,16), createdBy:'minus.plus.yummy',starred:'false',expiresIn:'100',messageCount:'999' };
+
+console.log(Octoberfest);
--- js/script.js
+++ js/script.js
@@ -5,28 +5,42 @@ console.log("App is alive");
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
+var currentChannel;
+var currentLocation ={longitude:33.873092,latitude:35.511176,what3words:"http://w3w.co/shack.slimmer.certainty"};
+
 function switchChannel(channelName) {
     //Log the channel switch
     console.log("Tuning in to channel", channelName);
 
+    currentChannel=channelName;
+
+    //Log the current channel name
+    console.log("currentChannel",currentChannel);
+    
+    //Log the current location
+    console.log("currentLocation",currentLocation);
+
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = channelName.name;
+    
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/'+channelName.createdBy+'"target="_blank"><strong>'+channelName.createdBy+'</strong></a>';
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    $('#channel-star').toggleClass((channelName.starred)?"fas far":"far fas");
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
     $('#channels li:contains(' + channelName + ')').addClass('selected');
+   // $('#channels li:contains(' + channelName.starred +''+ fas fa-star+')').toggleClass('fas far');
 }
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+   $('#channel-star').toggleClass("fas far");
+  
 }
 
 /**
@@ -50,4 +64,40 @@ function selectTab(tabId) {
 function toggleEmojis() {
     /* $('#emojis').show(); // #show */
     $('#emojis').toggle(); // #toggle
+}
+
+//constructor function to create new messages 
+function Message(createdBy,latitude,longitude,createdOn,expiresOn,own,text){
+
+/*this.createdBy='minus.plus.yummy'  ;  
+this.latitude=currentLocation.latitude;
+this.longitude=currentLocation.longitude;
+this.createdOn=Date.now();
+this.expiresOn=Date.now()+ 9e5;
+this.own=true;*/
+this.createdBy=createdBy  ;  
+this.latitude=latitude;
+this.longitude=longitude;
+this.createdOn=createdOn;
+this.expiresOn=expiresOn;
+this.own=own;
+this.text=text;
+
+}
+
+//create new message object form 
+function sendMessage(){
+
+    //var writtenmessage=$("input[name=inputmessage]").val();
+    var ownmessage=new Message("Hello Chatter");
+    console.log("newmessage",ownmessage);
+   $("<div class='message own'>").html(createMessageElement(ownmessage)).appendTo("#messages");
+}
+
+//function, which takes a message object and returns a string representation of an HTML message element
+function createMessageElement(objectmessage){
+
+return '<h3><a href="http://w3w.co/'+objectmessage.createdBy+'"target="_blank"><strong>'+objectmessage.createdBy+'</strong></a>'+objectmessage.createdOn+'<em>:expireIn:min. left</em></h3><p>'+ objectmessage.text +'</p><button>+5 min.</button>';
+
+   
 }
